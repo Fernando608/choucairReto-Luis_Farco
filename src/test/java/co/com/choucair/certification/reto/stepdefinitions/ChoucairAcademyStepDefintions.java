0809e5a367a7958959fbf34d7e975b52a40ec701
Fernando608 <luisfernandofo2015@gmail.com>
2021-11-21 09:17:15 -0500

    Abre el navegador y se dirige al página destino

---
+++ src/test/java/co/com/choucair/certification/reto/stepdefinitions/ChoucairAcademyStepDefintions.java
@@ -0,0 +1,33 @@
+package co.com.choucair.certification.reto.stepdefinitions;
+
+import co.com.choucair.certification.reto.task.OpenUp;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+public class ChoucairAcademyStepDefintions {
+    @Before
+    public void setStage(){
+        OnStage.setTheStage(new OnlineCast());
+    }
+
+    @Given("^Luis quiere registrarse en el sitio Utest$")
+    public void luisQuiereRegistrarseEnElSitioUtest() {
+        OnStage.theActorCalled("Luis").wasAbleTo(OpenUp.thePage());
+
+    }
+
+    @When("^inicia el proceso de registro en Utest$")
+    public void iniciaElProcesoDeRegistroEnUtest() {
+
+    }
+
+    @Then("^verifica el proceso final de registro$")
+    public void verificaElProcesoFinalDeRegistro() {
+
+    }
+
+}
---
+++ src/test/resources/features/academyChoucair.feature
@@ -0,0 +1,13 @@
+# Autor: Luis Farco Oviedo
+
+@stories
+Feature: Academy Choucair
+  Como usuario quiero registrarme en el sitio utest y realizar automatizaciones en diversos proyectos
+
+  @scenarion1
+  Scenario: Registrar en el sitio Utest
+    Given Luis quiere registrarse en el sitio Utest
+
+    When  inicia el proceso de registro en Utest
+
+    Then  verifica el proceso final de registro
